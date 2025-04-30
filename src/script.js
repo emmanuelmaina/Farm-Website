f0fbc5f127b277d34d4c6dc11fe302d1d79a2859
emmanuelmaina <irunguemmanuel808@gmail.com>
2025-05-01 01:22:24 +0300

    add main in inventory.html

---
+++ src/script.js
@@ -0,0 +1,123 @@
+// const addItem = document.getElementById("addItem");
+// const addButton = document.getElementById("add-item-button");
+// const  inventoryList = document.getElementById("inventoryList");
+
+
+
+// function addItemToInventory() {
+//     const item = addItem.value.trim();
+
+//     if (item) {
+//         createItemElement(item);
+//         addItem.value = "";
+//     }else {
+//         alert("Please enter an item name.");
+//     }
+// }
+
+// addButton.addEventListener("click", addItemToInventory);
+
+// function createItemElement(item) {
+//     const inventoryDiv = document.createElement("div");
+//     inventoryDiv.className = "inventoryList";
+
+//     const label = document.createElement("label");
+//     label.textContent = item;
+
+//     const add = document.createElement("button");
+//     add.id = "add";
+//     add.textContent = "+";
+    
+//     const subtract = document.createElement("button");
+//     subtract.id = "subtract";
+//     subtract.textContent = "-";
+
+//     const quantity = document.createElement("p");
+//     quantity.textContent = "0";
+
+//     inventoryDiv.appendChild(label);
+//     inventoryDiv.appendChild(add);
+//     inventoryDiv.appendChild(subtract);
+//     inventoryDiv.appendChild(quantity);
+
+//     document.body.appendChild(inventoryDiv);
+// }
+// function saveItems() {
+//     const items = [];
+//      inventoryList.querySelectorAll(".inventoryList").forEach(function(item) => {
+//         const itemName = item.querySelector("label").textContent;
+//         const itemQuantity = item.querySelector("p").textContent;
+//         items.push({ name: itemName, quantity: itemQuantity });        
+
+// })
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
