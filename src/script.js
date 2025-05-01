78df20c22d00ab358424b6e15f3a4782b0561a09
emmanuelmaina <irunguemmanuel808@gmail.com>
2025-05-01 12:35:42 +0300

    add event listeners to contact.html

--- src/script.js
+++ src/script.js
@@ -1,6 +1,55 @@
 // const addItem = document.getElementById("add");
 // const subtract = document.getElementById("subtract");
 // const result = document.getElementById("result")
+const myName = document.getElementById("name");
+
+myName.addEventListener('focus', () => {
+    myName.style.backgroundColor = "hsla(236, 85%, 16%, 0.466)";
+});   
+myName.addEventListener('focusout', () => {
+    myName.style.backgroundColor = "white";
+});   
+
+const myEmail = document.getElementById("email");
+
+myEmail.addEventListener('focus', () => {
+    myEmail.style.backgroundColor = "hsla(236, 85%, 16%, 0.466)";
+});   
+myEmail.addEventListener('focusout', () => {
+    myEmail.style.backgroundColor = "white";
+});   
+
+const myMessage = document.getElementById("message");
+
+myMessage.addEventListener('focus', () => {
+    myMessage.style.backgroundColor = "hsla(236, 85%, 16%, 0.466)";
+});   
+myMessage.addEventListener('focusout', () => {
+    myMessage.style.backgroundColor = "white";
+});   
+
+const submit = document.getElementById("btn");
+
+submit.addEventListener('mouseover', () => {
+    submit.style.backgroundColor = " hsl(193, 87%, 80%)";
+})
+submit.addEventListener('mouseout', () => {
+    submit.style.backgroundColor = "  hsl(193, 87%, 49%)";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 let result = 0;
 
@@ -18,7 +67,6 @@ document.getElementById("reset").addEventListener("click", function () {
     result = 0;
     document.getElementById("result").innerHTML = result;
 });
-
 
 
 
