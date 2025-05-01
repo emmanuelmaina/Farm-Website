a6ae38865ec463c69d06e1a8a73156b292744acf
emmanuelmaina <irunguemmanuel808@gmail.com>
2025-05-01 11:49:08 +0300

    add javascript logic

--- src/script.js
+++ src/script.js
@@ -1,71 +1,23 @@
-// const addItem = document.getElementById("addItem");
-// const addButton = document.getElementById("add-item-button");
-// const  inventoryList = document.getElementById("inventoryList");
+// const addItem = document.getElementById("add");
+// const subtract = document.getElementById("subtract");
+// const result = document.getElementById("result")
 
+let result = 0;
 
+document.getElementById("add").addEventListener("click", function () {
+  result += 1;
+  document.getElementById("result").innerHTML = result;
+});
 
-// function addItemToInventory() {
-//     const item = addItem.value.trim();
+document.getElementById("subtract").addEventListener("click", function () {
+  result -= 1;
+  document.getElementById("result").innerHTML = result;
+});
 
-//     if (item) {
-//         createItemElement(item);
-//         addItem.value = "";
-//     }else {
-//         alert("Please enter an item name.");
-//     }
-// }
-
-// addButton.addEventListener("click", addItemToInventory);
-
-// function createItemElement(item) {
-//     const inventoryDiv = document.createElement("div");
-//     inventoryDiv.className = "inventoryList";
-
-//     const label = document.createElement("label");
-//     label.textContent = item;
-
-//     const add = document.createElement("button");
-//     add.id = "add";
-//     add.textContent = "+";
-    
-//     const subtract = document.createElement("button");
-//     subtract.id = "subtract";
-//     subtract.textContent = "-";
-
-//     const quantity = document.createElement("p");
-//     quantity.textContent = "0";
-
-//     inventoryDiv.appendChild(label);
-//     inventoryDiv.appendChild(add);
-//     inventoryDiv.appendChild(subtract);
-//     inventoryDiv.appendChild(quantity);
-
-//     document.body.appendChild(inventoryDiv);
-// }
-// function saveItems() {
-//     const items = [];
-//      inventoryList.querySelectorAll(".inventoryList").forEach(function(item) => {
-//         const itemName = item.querySelector("label").textContent;
-//         const itemQuantity = item.querySelector("p").textContent;
-//         items.push({ name: itemName, quantity: itemQuantity });        
-
-// })
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.getElementById("reset").addEventListener("click", function () {
+    result = 0;
+    document.getElementById("result").innerHTML = result;
+});
 
 
 
